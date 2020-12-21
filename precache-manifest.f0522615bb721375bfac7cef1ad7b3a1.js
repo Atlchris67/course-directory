1d0d7f8fccd03ac7a732370115322dcba64fab8b
Atlchris67 <atlchris@gmail.com>
2020-12-21 09:57:53 -0500

    Updates

---
+++ precache-manifest.f0522615bb721375bfac7cef1ad7b3a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "527751acb6022af06d5d",
+    "url": "/course-directory/static/js/runtime~main.527751ac.js"
+  },
+  {
+    "revision": "1790d49fa19304142c35",
+    "url": "/course-directory/static/js/main.1790d49f.chunk.js"
+  },
+  {
+    "revision": "f5ae2494a8dbd9f95f4b",
+    "url": "/course-directory/static/js/1.f5ae2494.chunk.js"
+  },
+  {
+    "revision": "1790d49fa19304142c35",
+    "url": "/course-directory/static/css/main.403c4d2f.chunk.css"
+  },
+  {
+    "revision": "d02f5ccbfb3e6ef66d2723bf7ea1c6b6",
+    "url": "/course-directory/index.html"
+  }
+];
